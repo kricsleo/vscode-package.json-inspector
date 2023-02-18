48f6b5a7bbe66ac21b57f40749dd84f67f16ef53
许圣峰 <kricsleo@163.com>
2023-02-18 16:25:14 +0800

    WIP: add hover lint

--- src/index.ts
+++ src/index.ts
@@ -4,60 +4,60 @@ import { constants } from 'fs'
 import { access } from 'fs/promises'
 
 export function activate(context: ExtensionContext) {
-  console.log('vscode-package.json234 inspector start');
-  window.showInformationMessage('Hello, vscode-package.json inspector!')
+  console.log('vscode-package.json-inspector start');
+  const selector = [{ language: 'json', pattern: '**/package.json'}, { language: 'jsonc', pattern: '**/package.json'}]
   context.subscriptions.push(
-    // provide right-click to node_modules/
-    languages.registerDefinitionProvider(
-      [{ language: 'json', pattern: '**/package.json'}, { language: 'jsonc', pattern: '**/package.json'}],
-      { provideDefinition }
-    ),
     // provide hover detail
-    // languages.registerHoverProvider('json', { provideHover })
+    languages.registerHoverProvider(selector, { provideHover }),
+    // provide right-click to node_modules/
+    languages.registerDefinitionProvider(selector, { provideDefinition }),
   )
 }
 
-async function provideDefinition(
-  document: TextDocument,
-  position: Position,
-  token: CancellationToken
-) {
-  if(!isPkgJSON(document.fileName)) {
+async function provideDefinition(document: TextDocument, position: Position) {
+  const dependency = await getDependencyPath(document, position)
+  if(!dependency || !dependency.exsit) {
     return null
   }
+  return new Location(
+    Uri.file(dependency.path),
+    new Position(0, 0)
+  );
+}
+
+async function provideHover(document: TextDocument, position: Position) {
+  const dependency = await getDependencyPath(document, position)
+  if(!dependency || !dependency.exsit) {
+    return null
+  }
+  const dependencyPkg = require(dependency.path)
+  const content =
+`${dependencyPkg.name}\n
+${dependencyPkg.description || 'No description.'}\n
+current version: ${dependencyPkg.version}
+`
+  return new Hover(content)
+}
+
+async function getDependencyPath(document: TextDocument, position: Position) {
+  // if(!isPkgJSON(document.fileName)) {
+  //   return null
+  // }
   const safeDependencyWordRange = document.getWordRangeAtPosition(
     position,
     // reg: "lodash", (including prefix and suffix quotation marks to make it more accurate)
     new RegExp('"(?:@[a-z0-9-*~][a-z0-9-*._~]*/)?[a-z0-9-~][a-z0-9-._~]*"')
   )
+  if(!safeDependencyWordRange) {
+    return null
+  }
   const dependencyName = document.getText(safeDependencyWordRange).replace(/"/g, '')
   const currentDir = dirname(document.fileName)
   const dependencyPkgJSON = `${currentDir}/node_modules/${dependencyName}/package.json`
   const isDependencyExsit = await access(dependencyPkgJSON, constants.R_OK)
     .then(() => true)
     .catch(() => false)
-    console.log('isDependencyExsit', dependencyName, dependencyPkgJSON, isDependencyExsit)
-  if(isDependencyExsit) {
-    return new Location(
-      Uri.file(dependencyPkgJSON),
-      new Position(0, 0)
-    );
-//     const dependencyPkg = await import(dependencyPkgJSON)
-//     const content =
-// `### ${dependencyPkg.name}
-// ${dependencyPkg.description || 'No description.'}
-// current version: ${dependencyPkg.version}
-// `
-//       return new Hover(content)
-  }
-}
-
-function provideHover(
-  document: TextDocument,
-  position: Position,
-  token: CancellationToken
-) {
-
+  return { path: dependencyPkgJSON, exsit: isDependencyExsit }
 }
 
 function isPkgJSON(filename: string) {
